97b6c011c83d44d753779b6881d8625f9a2ea5ca
Vincent Wong <Vincent.Wong@D2L.com>
2019-01-04 13:30:31 -0800

    Remove unused old CDN stuff

--- publishLocation.js
+++
@@ -1,5 +0,0 @@
-var publisherOptions = require('./package.json').config.frauPublisher;
-publisherOptions.devTag = process.env.TRAVIS_COMMIT || publisherOptions.devTagVar;
-publisherOptions.version = process.env.TRAVIS_TAG;
-var publisher = require('frau-publisher').lib(publisherOptions);
-console.log('build published to:', publisher.getLocation() + 'd2l-telemetry-browser-client.js'); //eslint-disable-line no-console
