e6c403f416656e1b45408bde9bf9daf9b21f278f
Candice Pang <Candice.Pang@D2L.com>
2021-05-12 09:53:29 -0700

    US127307 Added test to check that no requests are fired for null endpoints

--- test/index.js
+++ test/index.js
@@ -68,4 +68,36 @@ describe('test suite', () => {
 			});
 		});
 	});
+
+	describe('logUserEvent - with null endpoint', () => {
+		var fetch, client, event;
+
+		before(() => {
+			client = new Client({
+				endpoint: null
+			});
+
+			event = new TelemetryEvent();
+			const eventBody = new EventBody();
+			event.setBody(eventBody);
+
+			fetch = sinon.stub(window.d2lfetch, 'fetch');
+			var promise = Promise.resolve({
+				ok: true,
+				json: function() {
+					return Promise.resolve();
+				}
+			});
+			fetch.returns(promise);
+		});
+
+		after(() => {
+			fetch && fetch.restore();
+		});
+
+		it ('POSTs an event', () => {
+			client.logUserEvent(event);
+			expect(fetch.calledWith(sinon.match.has('method', 'POST'))).to.be.false;
+		});
+	});
 });
